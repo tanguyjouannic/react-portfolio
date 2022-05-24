954de2adbcafda6397a3f8b8f32400da451bcc29
Tanguy Jouannic <tanguy.jouannic@gmail.com>
2022-05-24 18:09:22 +0200

    Add images and create navbar skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import { About, Footer, Header, Skills, Testimonial, Work } from './container/';
-import { Navbar } from './components';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import Navbar from './components/Navbar/Navbar';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Work />
